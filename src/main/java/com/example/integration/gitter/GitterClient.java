61b504110351a25b860971439d07c30f0fc36cb3
Olivier Dupré <odupre@astek.fr>
2018-03-24 00:09:36 +0100

    Fixed new builder API call for WebClient

--- src/main/java/com/example/integration/gitter/GitterClient.java
+++ src/main/java/com/example/integration/gitter/GitterClient.java
@@ -17,55 +17,57 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Component
 public class GitterClient {
 
-	private final WebClient webClient;
+    private final WebClient webClient;
 
-	public GitterClient(DashboardProperties properties) {
-		this.webClient = WebClient.create()
-				.filter(oAuthToken(properties.getGitter().getToken()));
-	}
+    public GitterClient(DashboardProperties properties) {
+        this.webClient = WebClient.create()
+                .mutate()
+                .filter(oAuthToken(properties.getGitter().getToken()))
+                .build();
+    }
 
-	public Flux<GitterUser> getUsersInRoom(String roomId, int limit) {
-		return this.webClient
-				.get().uri("https://api.gitter.im/v1/rooms/{roomId}/users?limit={limit}", roomId, limit)
-				.accept(MediaType.APPLICATION_JSON)
-				.exchange()
-				.flatMap(response -> response.bodyToFlux(GitterUser.class));
-	}
+    public Flux<GitterUser> getUsersInRoom(String roomId, int limit) {
+        return this.webClient
+                .get().uri("https://api.gitter.im/v1/rooms/{roomId}/users?limit={limit}", roomId, limit)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(response -> response.bodyToFlux(GitterUser.class));
+    }
 
-	public Mono<GitterUser> findUserInRoom(String userName, String roomId) {
-		return this.webClient
-				.get()
-				.uri("https://api.gitter.im/v1/rooms/{roomId}/users?q={userName}", roomId, userName)
-				.accept(MediaType.APPLICATION_JSON)
-				.exchange()
-				.then(response -> response.bodyToMono(GitterUser.class));
-	}
+    public Mono<GitterUser> findUserInRoom(String userName, String roomId) {
+        return this.webClient
+                .get()
+                .uri("https://api.gitter.im/v1/rooms/{roomId}/users?q={userName}", roomId, userName)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .then(response -> response.bodyToMono(GitterUser.class));
+    }
 
-	public Flux<GitterMessage> latestChatMessages(String roomId, int limit) {
-		return this.webClient
-				.get()
-				.uri("https://api.gitter.im/v1/rooms/{roomId}/chatMessages?limit={limit}", roomId, limit)
-				.accept(MediaType.APPLICATION_JSON)
-				.exchange()
-				.flatMap(response -> response.bodyToFlux(GitterMessage.class));
-	}
+    public Flux<GitterMessage> latestChatMessages(String roomId, int limit) {
+        return this.webClient
+                .get()
+                .uri("https://api.gitter.im/v1/rooms/{roomId}/chatMessages?limit={limit}", roomId, limit)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(response -> response.bodyToFlux(GitterMessage.class));
+    }
 
-	public Flux<GitterMessage> streamChatMessages(String roomId) {
-		return this.webClient
-				.get()
-				.uri("https://stream.gitter.im/v1/rooms/{roomId}/chatMessages", roomId)
-				.accept(MediaType.TEXT_EVENT_STREAM)
-				.exchange()
-				.flatMap(response -> response.bodyToFlux(GitterMessage.class));
-	}
+    public Flux<GitterMessage> streamChatMessages(String roomId) {
+        return this.webClient
+                .get()
+                .uri("https://stream.gitter.im/v1/rooms/{roomId}/chatMessages", roomId)
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .flatMap(response -> response.bodyToFlux(GitterMessage.class));
+    }
 
-	private ExchangeFilterFunction oAuthToken(String token) {
-		return (clientRequest, exchangeFunction) ->
-				exchangeFunction
-						.exchange(ClientRequest
-								.from(clientRequest)
-								.header("Authorization", "Bearer " + token)
-								.build()
-						);
-	}
+    private ExchangeFilterFunction oAuthToken(String token) {
+        return (clientRequest, exchangeFunction)
+                -> exchangeFunction
+                .exchange(ClientRequest
+                        .from(clientRequest)
+                        .header("Authorization", "Bearer " + token)
+                        .build()
+                );
+    }
 }
